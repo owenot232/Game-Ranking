486a5487f531d1825bdc5759928c69dea5f27024
owenot232 <43624865+owenot232@users.noreply.github.com>
2026-04-06 19:34:50 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'game-ranker-v11';
+const CACHE_NAME = 'game-ranker-v12';
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['index.html', 'manifest.json'])));
 });
